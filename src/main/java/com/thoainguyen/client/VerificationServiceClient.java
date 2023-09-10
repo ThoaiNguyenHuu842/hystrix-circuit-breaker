96a7478d1822548c3bf734687674856f92a8426c
LAPTOP-0OHSO3Q8\thoai <thoainguyen842@gmail.com>
2023-09-10 12:31:47 +0700

    code clean

--- src/main/java/com/thoainguyen/client/VerificationServiceClient.java
+++ src/main/java/com/thoainguyen/client/VerificationServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(value = "VerificationServiceClient", url = "${key-service.url}", fallback = VerificationServiceClientFallback.class)
+@FeignClient(value = "VerificationServiceClient", url = "${kyc-service.url}", fallback = VerificationServiceClientFallback.class)
 public interface VerificationServiceClient {
   @PostMapping(value = "/kyc/{userName}", produces = MediaType.APPLICATION_JSON_VALUE)
   String verify(@PathVariable String userName);
